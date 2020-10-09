cfc3784ebeb548924f3e5c47a2c8ceafd2430e92
Joshua Stay <57918075+joshuastay@users.noreply.github.com>
2020-10-08 20:47:05 -0700

    Add files via upload

---
+++ Exercise4/Exercise4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter First altitude: ");
+
+            int firstAltitude = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Second altitude: ");
+
+            int secondAltitude = int.Parse(Console.ReadLine());
+
+            int differenceAltitudes = firstAltitude - secondAltitude;
+
+            if (differenceAltitudes < 0)
+            {
+                differenceAltitudes = differenceAltitudes * -1;
+
+                Console.WriteLine("Change in Altitude is: " + differenceAltitudes);
+
+
+            }
+            else
+            {
+                Console.WriteLine("Change in Altitude is : " + differenceAltitudes);
+            }
+        }
+    }
+}
