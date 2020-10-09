876e0c6d1cdb4f66a83736687e9659903d6203b9
Joshua Stay <57918075+joshuastay@users.noreply.github.com>
2020-10-08 20:48:55 -0700

    Add files via upload

---
+++ AgeExercise/AgeExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AgeExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create variable to store age
+
+            int userAge = 32;
+
+            Console.WriteLine(userAge);
+        }
+    }
+}
---
+++ FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalSecondsPlayed = 200;
+
+            const int SecondsPerMinute = 60;
+
+            // convert seconds to minutes
+
+            int minutesPlayed = totalSecondsPlayed / SecondsPerMinute;
+
+            int secondsLeftPlayed = totalSecondsPlayed % SecondsPerMinute;
+
+            // print variables to console
+
+            Console.WriteLine("Minutes: " + minutesPlayed);
+
+            Console.WriteLine("Seconds: " + secondsLeftPlayed);
+        }
+    }
+}
