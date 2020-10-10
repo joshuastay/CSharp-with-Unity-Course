17a8acc215cb038379adf8e26ca9e8afe06f464c
Joshua Stay <57918075+joshuastay@users.noreply.github.com>
2020-10-09 18:48:06 -0700

    Add files via upload

---
+++ Exercise6/Exercise6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int degreesAngle = 56;
+
+            float cosine = (float)Math.Cos(degreesAngle * Math.PI / 180);
+
+            float sine = (float)Math.Sin(degreesAngle * Math.PI / 180);
+
+            Console.WriteLine("The cosine of 56 degrees is: " + cosine);
+
+            Console.WriteLine("The sine of 56 degrees is: " + sine);
+
+            Console.WriteLine();
+
+        }
+    }
+}
