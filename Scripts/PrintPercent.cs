9dfda97d50b8c62111f9f3a19785953a55c18380
Joshua Stay <57918075+joshuastay@users.noreply.github.com>
2020-10-09 18:28:38 -0700

    Add files via upload

---
+++ Scripts/PrintPercent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintPercent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        const int MaxScore = 100;
+
+        int score = 56;
+
+        float percent = (float)score / MaxScore;
+
+        float percentConvert = percent * 100;
+
+        print("Score percent is: " + percentConvert + "%");
+    }
+}
